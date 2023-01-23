9b76e4cc7c93d4c94dbc1ac8d92fac7605dc7fe8
canberkarkose <canberk.arkose@outlook.com>
2023-01-24 00:37:45 +0300

    Basic Table Update

---
+++ src/components/columns.js
@@ -0,0 +1,14 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Brand',
+        accessor: 'brand_name'
+    },
+    {
+        Header: 'Price',
+        accessor: 'price'
+    },
+]
